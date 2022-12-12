5a75d8ac71ce95fcd59410bcc1c271e55740d76d
mrsamia <15203060@iubat.edu>
2022-12-12 10:55:45 +0600

    SRM | ScrollToTop

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './Components/ScrollToTop';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
    <BrowserRouter>
+   <ScrollToTop />
     <App />
     </BrowserRouter>
   </React.StrictMode>
